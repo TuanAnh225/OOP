5228715f82bd0ca640ad5baa7996d835aef67007
TuanAnh225 <tuananhchums23@gmail.com>
2022-11-09 13:29:08 +0700

    homeworklab2

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -16,4 +16,25 @@ public class Cart {
 		}
 	}
 	
+	public static float totalCost() {
+		float sum=0;
+		for(int i=0;i<qtyOrdered;i++) {
+			sum=sum+itemsOrdered[i].getCost();
+		}
+		return sum;
+	}
+	public static void removeDigitalVideoDisc(DigitalVideoDisc dvd) {
+		int position=0;
+		if(qtyOrdered==0) System.out.println("Remove Failed!");
+		else {
+		for(int i=0;i<qtyOrdered;i++) {
+			if(itemsOrdered[i].equals(dvd)) {position = i;  break;}
+		}
+		for(int i=position;i<qtyOrdered-1;i++) {
+			itemsOrdered[i]=itemsOrdered[i+1];
+		}
+		System.out.println("Remove Successfull!");
+		qtyOrdered--;
+		}
+	}
 }
